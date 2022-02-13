1d05b9be93688aeeac2d47a7fae43e857ba77341
Renan Pereira <renan.souza.dev@gmail.com>
2022-02-13 16:11:55 -0300

    feat(components): create Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react'
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps
+} from '@chakra-ui/react'
+import { motion, MotionProps } from 'framer-motion'
+
+type AnimatedButtonProps = ChakraButtonProps & MotionProps
+
+export type ButtonProps = AnimatedButtonProps & {
+  children: ReactNode
+}
+
+const MotionButton = motion<AnimatedButtonProps>(ChakraButton)
+
+export const Button = ({ children, ...rest }: ButtonProps) => {
+  return <MotionButton {...rest}>{children}</MotionButton>
+}
