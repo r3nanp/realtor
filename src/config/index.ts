0463cd6e3ac148fb6801a095d4ffd9c4ac435a04
Renan Pereira <renan.souza.dev@gmail.com>
2022-02-13 16:12:12 -0300

    feat(config): add config urls

---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+export const API_URL = process.env.API_URL
+export const API_KEY = process.env.API_KEY
+export const API_HOST = process.env.API_HOST
+
+export const BANNER_URL =
+  'https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
