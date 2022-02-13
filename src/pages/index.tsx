586ecd8ccefd38f7a7cc0b9db23642956ef8ed07
Renan Pereira <renan.souza.dev@gmail.com>
2022-02-13 16:12:28 -0300

    feat(pages): add banner to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
 import type { NextPage } from 'next'
-import { Heading } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+
+//* CUSTOM IMPORTS
+import { Banner } from 'components'
+import { BANNER_URL } from 'config'
 
 const Home: NextPage = () => {
-  return <Heading>Hi!</Heading>
+  return (
+    <Box as="main">
+      <Banner
+        purpose="RENT A HOME"
+        title="Rental homes for"
+        subTitle="everyone"
+        description="Explore Apartments, Villas, Homes"
+        subDescription="and more"
+        buttonText="Explore Renting"
+        href="/search?purpose=for-rent"
+        imageUrl={BANNER_URL}
+      />
+
+      <Banner
+        purpose="BUY A HOME"
+        title="Find, Buy &nbsp; Own Your"
+        subTitle="Dream Home"
+        description="Explore Apartments, Villas, Homes"
+        subDescription="and more"
+        buttonText="Explore Buying"
+        href="/search?purpose=for-sale"
+        imageUrl={BANNER_URL}
+      />
+    </Box>
+  )
 }
 
 export default Home
